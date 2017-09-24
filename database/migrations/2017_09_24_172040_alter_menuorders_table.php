b4df239a0ad5abf41cb4e62be13245c5d58b8437
Abhijeet Khairnar <abhijeetkhairnar@Abhijeets-MacBook-Pro.local>
2017-09-24 22:52:35 +0530

    alter order_no

---
+++ database/migrations/2017_09_24_172040_alter_menuorders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterMenuordersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('menuorders', function (Blueprint $table) {
+            //
+            $table->integer('order_no')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('menuorders', function (Blueprint $table) {
+            //
+            $table->dropColumn('order_no');
+        });
+    }
+}
