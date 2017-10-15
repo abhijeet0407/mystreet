f1fa417c01ecce33f1a324703676620c06a507bc
Abhijeet Khairnar <abhijeetkhairnar@Abhijeets-MacBook-Pro.local>
2017-10-15 17:39:51 +0530

    rsaccavenue8

--- app/Http/Controllers/CustomerLoginController.php
+++ app/Http/Controllers/CustomerLoginController.php
@@ -32,8 +32,8 @@ class CustomerLoginController extends Controller
         $url = "https://secure.ccavenue.com/transaction/getRSAKey";
         $fields = array(
                 'access_code'=>"AVFV72EG00BL93VFLB",
-                'order_id'=>'23'
-               // 'order_id'=>$request['order_id']
+                //'order_id'=>'23'
+               'order_id'=>$request['order_id']
         );
        // print_r($fields);
 
