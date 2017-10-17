dc0ce087dce69aa3b18afffc69affc8c549fd494
Abhijeet Khairnar <abhijeetkhairnar@Abhijeets-MacBook-Pro.local>
2017-10-18 01:55:49 +0530

    rsaccavenue14

--- app/Http/Controllers/CustomerLoginController.php
+++ app/Http/Controllers/CustomerLoginController.php
@@ -295,4 +295,133 @@ mail($to2,$subject2,$message,$headers);
 
 
     }
+
+
+
+
+
+
+    /**/
+
+
+    function encrypt($plainText,$key)
+    {
+        $secretKey = hextobin(md5($key));
+        $initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
+        $openMode = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '','cbc', '');
+        $blockSize = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, 'cbc');
+        $plainPad = pkcs5_pad($plainText, $blockSize);
+        if (mcrypt_generic_init($openMode, $secretKey, $initVector) != -1) 
+        {
+              $encryptedText = mcrypt_generic($openMode, $plainPad);
+                  mcrypt_generic_deinit($openMode);
+                        
+        } 
+        return bin2hex($encryptedText);
+    }
+
+    function decrypt($encryptedText,$key)
+    {
+        $secretKey = hextobin(md5($key));
+        $initVector = pack("C*", 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f);
+        $encryptedText=hextobin($encryptedText);
+        $openMode = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '','cbc', '');
+        mcrypt_generic_init($openMode, $secretKey, $initVector);
+        $decryptedText = mdecrypt_generic($openMode, $encryptedText);
+        $decryptedText = rtrim($decryptedText, "\0");
+        mcrypt_generic_deinit($openMode);
+        return $decryptedText;
+        
+    }
+    //*********** Padding Function *********************
+
+     function pkcs5_pad ($plainText, $blockSize)
+    {
+        $pad = $blockSize - (strlen($plainText) % $blockSize);
+        return $plainText . str_repeat(chr($pad), $pad);
+    }
+
+    //********** Hexadecimal to Binary function for php 4.0 version ********
+
+    function hextobin($hexString) 
+     { 
+            $length = strlen($hexString); 
+            $binString="";   
+            $count=0; 
+            while($count<$length) 
+            {       
+                $subString =substr($hexString,$count,2);           
+                $packedString = pack("H*",$subString); 
+                if ($count==0)
+            {
+                $binString=$packedString;
+            } 
+                
+            else 
+            {
+                $binString.=$packedString;
+            } 
+                
+            $count+=2; 
+            } 
+            return $binString; 
+          } 
+
+
+
+
+    /**/
+
+
+
+    /**/
+    function SuccessCart(Request $request){
+        $workingKey='A33EC01955E79CC8261D05573806F75A';     //Working Key should be provided here.
+    $encResponse=$request["encResp"];         //This is the response sent by the CCAvenue Server
+    $rcvdString=decrypt($encResponse,$workingKey);      //Crypto Decryption used as per the specified working key.
+    $order_status="";
+    $decryptValues=explode('&', $rcvdString);
+    $dataSize=sizeof($decryptValues);
+    echo "<center>";
+
+    for($i = 0; $i < $dataSize; $i++) 
+    {
+        $information=explode('=',$decryptValues[$i]);
+        if($i==3)   $order_status=$information[1];
+    }
+
+    if($order_status==="Success")
+    {
+        
+        echo "<br>Thank you for shopping with us. Your credit card has been charged and your transaction is successful. We will be shipping your order to you soon.";
+        
+    }
+    else if($order_status==="Aborted")
+    {
+        echo "<br>Thank you for shopping with us.We will keep you posted regarding the status of your order through e-mail";
+    
+    }
+    else if($order_status==="Failure")
+    {
+        echo "<br>Thank you for shopping with us.However,the transaction has been declined.";
+    }
+    else
+    {
+        echo "<br>Security Error. Illegal access detected";
+    
+    }
+
+    echo "<br><br>";
+
+    echo "<table cellspacing=4 cellpadding=4>";
+    for($i = 0; $i < $dataSize; $i++) 
+    {
+        $information=explode('=',$decryptValues[$i]);
+            echo '<tr><td>'.$information[0].'</td><td>'.$information[1].'</td></tr>';
+    }
+
+    echo "</table><br>";
+    echo "</center>";
+    }
+    /**/
 }
